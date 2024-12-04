37676595c6d86890f646c4d14f4893e356f92a17
Matt-Elias <emb6117@gmail.com>
2024-12-04 11:12:07 -0600

    Primer subida

---
+++ app/src/main/java/com/example/myapplication/IniciarSesion_Activity.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class IniciarSesion_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_iniciar_sesion)
+
+    }
+}
