9d7dbedabb46ad524a544ca8bd2bf7ef63da4576
F3rny <20233tn099utez.edu.mx>
2024-12-11 23:58:08 -0600

    Arreglando las versiones del proyecto y agregando dependecias de auth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.myapplication"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.myapplication"
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -56,9 +56,16 @@ dependencies {
 
     //Dependencias de FIREBASE
     implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+    implementation("com.android.volley:volley:1.2.1")
     implementation("com.google.firebase:firebase-database")
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-messaging:24.1.0")
 
-    //Dependencia para notificaciones:
-    implementation("androidx.core:core-ktx:2.2.0")
+
 
 }
